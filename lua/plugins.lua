1ad61b6d0b8991a6fa4577a10c013f5fae77176d
Ben Sykes <hootieben@gmail.com>
2021-12-26 09:11:36 -0600

    Fresh start

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,176 +1,36 @@
+local utils = require("utils")
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+
+vim.g.package_home = fn.stdpath("data") .. "/site/pack/packer/"
+local packer_install_dir = vim.g.package_home .. "/opt/packer.nvim"
+
+local plug_url_format = "https://github.com/%s"
+local packer_repo = string.format(plug_url_format, "wbthomason/packer.nvim")
+
+local install_cmd = string.format("10split |term git clone --depth=1 %s %s", packer_repo, packer_install_dir)
+
+-- Auto-install packer in case it hasn't been installed.
+if fn.glob(packer_install_dir) == "" then
+  vim.api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
+  vim.cmd(install_cmd)
 end
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
+-- Load packer.nvim
+vim.cmd("packadd packer.nvim")
+local util = require('packer.util')
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+require("packer").startup({
+  function(use)
+    -- it is recommened to put impatient.nvim before any other plugins
+    use {'lewis6991/impatient.nvim', config = [[require('impatient')]]}
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    requires = {
-      {
-        "windwp/nvim-autopairs",
-        run = "make",
-        config = function()
-          require("nvim-autopairs").setup {}
-        end,
-      },
+    use({"wbthomason/packer.nvim", opt = true})
+  end,
+  config = {
+    max_jobs = 16,
+    compile_path = util.join_paths(vim.fn.stdpath('config'), 'lua', 'packer_compiled.lua'),
+    git = {
+      default_url_format = plug_url_format,
     },
-  }
-
-  -- LSP
-  use {
-    'onsails/lspkind-nvim',
-    'neovim/nvim-lspconfig',
-    'williamboman/nvim-lsp-installer'
-  }
-
-  -- Pretty symbols
-  use 'kyazdani42/nvim-web-devicons'
-
-  -- Trouble
-  use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
-
-  -- Indentation
-  use 'lukas-reineke/indent-blankline.nvim'
-
-  -- testing
-  use { 'vim-test/vim-test'}
-
-  -- Search
-  use {
-    {
-      'nvim-telescope/telescope.nvim',
-      requires = {
-        'nvim-lua/popup.nvim',
-        'nvim-lua/plenary.nvim',
-        'telescope-frecency.nvim',
-        'telescope-fzf-native.nvim',
-      },
-      wants = {
-        'popup.nvim',
-        'plenary.nvim',
-        'telescope-frecency.nvim',
-        'telescope-fzf-native.nvim',
-      },
-      setup = [[require('config.telescope_setup')]],
-      config = [[require('config.telescope')]],
-      cmd = 'Telescope',
-      module = 'telescope',
-    },
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      run = 'make',
-    },
-  }
-
-  use {
-    "nvim-telescope/telescope-frecency.nvim",
-    config = function()
-      require"telescope".load_extension("frecency")
-    end,
-    requires = {"tami5/sqlite.lua"}
-  }
-
-  -- Git
-  use { 'tpope/vim-fugitive' }
-
-  use {
-    'tpope/vim-rhubarb',
-    requires = 'tpope/vim-fugitive',
-  }
-
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    },
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
-
-  -- go
-  use { 'fatih/vim-go', run = ':GoUpdateBinaries' }
-  use { 'sebdah/vim-delve' }
-
-  -- Color scheme
-  use {
-    'dracula/vim',
-    as = 'dracula'
-  }
-
-  -- Lualine
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
-
-  -- Completion
-  use {
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-nvim-lsp",
-      "quangnguyen30192/cmp-nvim-ultisnips",
-      "hrsh7th/cmp-nvim-lua",
-      "octaltree/cmp-look",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-calc",
-      "f3fora/cmp-spell",
-      "hrsh7th/cmp-emoji",
-      "ray-x/cmp-treesitter",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-nvim-lsp-document-symbol",
-    },
-    config = function()
-      require("config.cmp").setup()
-    end,
-  }
-
-  use {
-    "SirVer/ultisnips",
-    requires = { { "honza/vim-snippets", rtp = "." }, "mlaursen/vim-react-snippets" },
-    config = function()
-      vim.g.UltiSnipsExpandTrigger = "<Plug>(ultisnips_expand)"
-      vim.g.UltiSnipsJumpForwardTrigger = "<Plug>(ultisnips_jump_forward)"
-      vim.g.UltiSnipsJumpBackwardTrigger = "<Plug>(ultisnips_jump_backward)"
-      vim.g.UltiSnipsListSnippets = "<c-x><c-s>"
-    end,
-  }
-
-  -- Kitty Integration
-  use { 'knubie/vim-kitty-navigator' }
-
-  -- Direnv
-  use { 'direnv/direnv.vim'}
-
-  -- Kommentary
-  use { 'b3nj5m1n/kommentary'}
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  },
+})
