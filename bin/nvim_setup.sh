147ea66575fe846ac885c995a7b8da494248cfd9
Ben Sykes <hootieben@gmail.com>
2022-03-05 07:52:21 -0600

    Add shellcheck directives

--- bin/nvim_setup.sh
+++ bin/nvim_setup.sh
@@ -9,13 +9,16 @@ if [[ ! -d "$NVIMVENV" ]]; then
   python3 -m venv "$HOME/.venv/nvim"
 fi
 
+# shellcheck source=/home/hootieben/.venv/bin/activate
 source "$NVIMVENV/bin/activate"
 python -m pip install --upgrade pynvim
 
 echo "Installing nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
+# shellcheck source=/home/hootieben/.nvm/nvm.sh
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# shellcheck source=/home/hootieben/.nvm/nvm.sh
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install 16
